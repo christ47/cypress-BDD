3fdec4d2beb09f6ff9bcd3f8d9108b68dd6d9608
christ47 <imani.c.christie@gmail.com>
2024-08-03 21:41:17 +0100

    Fixtures updated

--- cypress/e2e/customerJourney/checkoutpage.ts
+++ cypress/e2e/customerJourney/checkoutpage.ts
@@ -1,15 +1,21 @@
 import { When , Then } from "@badeball/cypress-cucumber-preprocessor"
 import "@testing-library/cypress/add-commands";
 
+
 Then(/^I see confirmation of my order$/, () => {
     cy.findByText(/Thank you for your order!/).should('be.visible')
     cy.findByText(/Your order has been dispatched, and will arrive just as fast as the pony can get there!/).should('be.visible')
 });
 
 When(/^I add my checkout information$/, () => {
-	cy.get('#first-name').type('Maxwell')
-    cy.get('#last-name').type('Brown')
-    cy.get('#postal-code').type('SE1 0JD')
+    
+    cy.fixture('user').then((user)=>{
+        cy.get('#first-name').type(user.firstName)
+        cy.get('#last-name').type(user.lastName)
+        cy.get('#postal-code').type(user.postCode)
+    })
+    cy.screenshot()
+
 });
 
 When(/^I enter my checkout information$/, () => {
